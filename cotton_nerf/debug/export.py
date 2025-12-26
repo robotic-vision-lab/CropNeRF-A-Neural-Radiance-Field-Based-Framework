886e5cf603f739ac9ba8a1d02c03fbbb7ce2faf5
William J. Beksi <william.beksi@uta.edu>
2025-12-26 16:02:24 -0500

    Rename old files.

--- cotton_nerf/debug/export.py
+++
@@ -1,6 +0,0 @@
-import re
-import sys
-from fruit_nerf.scripts.exporter import entrypoint
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(entrypoint())
